66a8813a32a08d7d7b0e319d9934684c8eb05d99
Oskar <oskar.case@gmail.com>
2020-02-07 16:37:29 +0800

    Created a custom form for creating an article

--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Articles from '../components/Article';
+import CustomForm from '../components/Form';
 import axios from 'axios';
 
 
@@ -23,7 +24,12 @@ class ArticleList extends React.Component {
 
     render() {
         return (
-            <Articles data={this.state.articles} />
+            <div>
+                <Articles data={this.state.articles} />
+                <br />
+                <h2>Create an article</h2>
+                <CustomForm />
+            </div>
         )
     }
 }
