874a0a31df15cf21f92b317e4209d1a9c71f13c1
Oskar <oskar.case@gmail.com>
2020-02-15 16:48:52 +0800

    Updated index.js

--- frontend/gui/src/index.js
+++ frontend/gui/src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
 
 import reducer from './store/reducers/auth';
 
-const store = createStore(reducer);
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhances(
+    applyMiddleware(thunk)
+));
+
+const app = (
+    <Provider>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
